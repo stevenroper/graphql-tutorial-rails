ef56224f6f3eab8f5a58604a54f3856e6fccc7b5
Steven Roper <sroper@uesp.org>
2019-02-21 16:30:27 -0700

    Create votes for links and tie them all together.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,8 @@ module Types
   class UserType < BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :votes, [VoteType], null: false
+    field :links, [LinkType], null: false
 
     # This is here just for testing purposes
     field :email, String, null: false
